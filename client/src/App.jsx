6edc8e0c3cfa86943d4e81a61cacb1401ffede06
putraaxzy <wibowo.yunanto41@smk.belajar.id>
2024-05-09 15:39:32 +0700

    quick fix up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,11 @@ import Login from "./routes/login/login";
 import Register from "./routes/register/register";
 import ProfileUpdatePage from "./routes/profileUpdatePage/profileUpdatePage";
 import NewPostPage from "./routes/newPostPage/newPostPage";
-import { listPageLoader, profilePageLoader, singlePageLoader } from "./lib/loaders";
+import {
+  listPageLoader,
+  profilePageLoader,
+  singlePageLoader,
+} from "./lib/loaders";
 
 function App() {
   const router = createBrowserRouter([
@@ -48,7 +52,7 @@ function App() {
         {
           path: "/profile",
           element: <ProfilePage />,
-          loader: profilePageLoader
+          loader: profilePageLoader,
         },
         {
           path: "/profile/update",
